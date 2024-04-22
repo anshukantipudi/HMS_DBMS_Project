257f54b579d113f6e1833c52894c0061f45016ce
Shash <shashwatharsh03@gmail.com>
2024-04-22 20:38:46 +0530

    form view table added

---
+++ view_tables.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace HMS
+{
+    partial class view_tables
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            ViewGrid_bt = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Book Antiqua", 24F, FontStyle.Bold | FontStyle.Italic);
+            label1.Location = new Point(141, 24);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(306, 49);
+            label1.TabIndex = 4;
+            label1.Text = "Execute Queries";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(310, 113);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(205, 28);
+            comboBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Book Antiqua", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(99, 113);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(152, 28);
+            label2.TabIndex = 6;
+            label2.Text = "Select Table :";
+            // 
+            // ViewGrid_bt
+            // 
+            ViewGrid_bt.Font = new Font("Book Antiqua", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ViewGrid_bt.Location = new Point(223, 160);
+            ViewGrid_bt.Margin = new Padding(2);
+            ViewGrid_bt.Name = "ViewGrid_bt";
+            ViewGrid_bt.Size = new Size(138, 37);
+            ViewGrid_bt.TabIndex = 7;
+            ViewGrid_bt.Text = "View";
+            ViewGrid_bt.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(47, 221);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(500, 350);
+            dataGridView1.TabIndex = 8;
+            // 
+            // view_tables
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Background_image;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(585, 609);
+            Controls.Add(dataGridView1);
+            Controls.Add(ViewGrid_bt);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Name = "view_tables";
+            Text = "view_tables";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Button ViewGrid_bt;
+        private DataGridView dataGridView1;
+    }
+}
